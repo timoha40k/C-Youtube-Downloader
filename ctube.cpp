7cbcf72789b86aef3e09152cf61baf4e0f6db13f
timoha40k <102353433+timoha40k@users.noreply.github.com>
2026-03-20 11:12:40 +0100

    Refactoring. Json parsing removed

--- ctube.cpp
+++ ctube.cpp
@@ -7,9 +7,7 @@
 #include <string>
 #include <iostream>
 #include <vector>
-#include "json.hpp"
-#include "convertor.hpp"
-
+#include "clients.hpp"
 // callback to collect response
 size_t write_to_variable(char* ptr, size_t size, size_t nmemb, std::string* data) {
     data->append(ptr, size * nmemb);
@@ -25,7 +23,7 @@ size_t write_to_file(void *data, size_t size, size_t nmemb, std::ofstream *userd
 void delete_unnessecary_output(std::string& output, const std::string& first_sub, const std::string& last_sub){
     size_t pos_begin = output.find(first_sub);
     if (pos_begin != std::string::npos)
-        output.erase(0, pos_begin + first_sub.size());// if found, delete
+        output.erase(0, pos_begin + first_sub.size());//if found, delete
     else
         std::cerr << "Error: first sub is not found" << std::endl;
 
@@ -76,76 +74,24 @@ std::string extract_videoId(const char* link){
     return videoId;
 }
 
-std::vector<std::string> get_audio_download_url(nlohmann::json& data, size_t adaptiveFormats_size){
-    std::vector<std::string> audio_urls;
-    for (size_t i = 0; i < adaptiveFormats_size; i++){
-        std::string mimeType = data["streamingData"]["adaptiveFormats"][i]["mimeType"];
-        size_t pos = mimeType.find("audio/");
-        if (pos == std::string::npos)
-            continue;
-        std::string audio_url = data["streamingData"]["adaptiveFormats"][i]["url"];
-        audio_urls.push_back(audio_url);
-    }
-    return audio_urls;
-}
-
-
-int main(int argc, char *argv[]) {
-    CURL* curl = curl_easy_init();
-    std::string response;
-
-    std::string body = R"({
-      "context": {
-        "client": {
-          "hl": "en",
-          "gl": "US",
-          "clientName": "ANDROID",
-          "clientVersion": "21.02.35",
-          "clientScreen": "WATCH",
-          "androidSdkVersion": 30,
-          "userAgent": "com.google.android.youtube/21.02.35 (Linux; U; Android 11) gzip",
-          "osName": "Android",
-          "osVersion": "11"
-        },
-        "thirdParty": {
-          "embedUrl": "https://www.youtube.com/"
-        }
-      },
-      "playbackContext": {
-        "contentPlaybackContext": {
-          "signatureTimestamp": 19250
-        }
-      },
-      "racyCheckOk": true,
-      "contentCheckOk": true,
-    )";
-
-    std::string body_tv = R"({
-        "context": {
-            "client": {
-                "hl": "en",
-                "gl": "US",
-                "clientName": "ANDROID_MUSIC",
-                "clientVersion": "5.26.1",
-                "clientScreen": "WATCH",
-                "androidSdkVersion": 31
-            }
-        },
-        "racyCheckOk": true,
-        "contentCheckOk": true,
-    )";
-
-    std::string videoId = extract_videoId(argv[1]);
+std::string get_body_to_video(Clients& clients, const char* link){
+    std::string body = clients.clients[0].second;
+    //std::cout << link << std::endl;
+    std::string videoId = extract_videoId(link);
     body += "  \"videoId\": \"";
     body += videoId;
     body += "\"\n}";
+    return body;
+}
 
-    struct curl_slist* headers = nullptr;
+void headers_to_player(curl_slist*& headers){
     headers = curl_slist_append(headers, "Content-Type: application/json");
     headers = curl_slist_append(headers, "User-Agent: com.google.android.apps.youtube.vr.oculus/1.71.26 (Linux; U; Android 12L; eureka-user Build/SQ3A.220605.009.A1) gzip");
     headers = curl_slist_append(headers, "X-YouTube-Client-Name: 28");
     headers = curl_slist_append(headers, "X-YouTube-Client-Version: 1.71.26");
+}
 
+void make_ytplayer_request(CURL*& curl, std::string& body, curl_slist*& headers, CURLcode& res, std::string& response){
     curl_easy_setopt(curl, CURLOPT_URL, "https://www.youtube.com/youtubei/v1/player");
     curl_easy_setopt(curl, CURLOPT_POST, 1L);
     curl_easy_setopt(curl, CURLOPT_POSTFIELDS, body.c_str());
@@ -153,7 +99,7 @@ int main(int argc, char *argv[]) {
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_to_variable);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
 
-    CURLcode res = curl_easy_perform(curl);
+    res = curl_easy_perform(curl);
 
     if (res != CURLE_OK)
         std::cerr << "curl error: " << curl_easy_strerror(res) << std::endl;
@@ -161,101 +107,72 @@ int main(int argc, char *argv[]) {
     curl_slist_free_all(headers);
 
     curl_easy_reset(curl);
+}
 
-    std::string video_url = get_download_url(response);
-    std::string video_title = get_video_title(response);
-    
+void curl_download(CURL*& curl, CURLcode& res, std::string& url, std::string& file_name){
+    std::ofstream video(file_name, std::ios::binary);
 
-    std::string vid_file = video_title;
-    vid_file += ".mp4";
-    std::ofstream video(vid_file, std::ios::binary);
 
-    struct curl_slist* download_headers = nullptr;
-    download_headers = curl_slist_append(download_headers, "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 Chrome/120.0.0.0 Safari/537.36");
-    download_headers = curl_slist_append(download_headers, "Referer: https://www.youtube.com/");
-
-    curl_easy_setopt(curl, CURLOPT_URL, video_url.c_str());
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_to_file);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &video);
-    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, download_headers);
-    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L); // follow redirects
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 
     res = curl_easy_perform(curl);
 
-    // check what actually happened
-    long httpCode = 0;
     if (res != CURLE_OK)
         std::cerr << "curl error: " << curl_easy_strerror(res) << std::endl;
 
-    curl_slist_free_all(download_headers);
-
 
     video.close();
+
+    curl_easy_reset(curl);
+}
+
+void convert_to_audio(std::string& video_name, std::string& audio_name){
+    std::string ffmpeg_command = "ffmpeg -i '";
+    ffmpeg_command += video_name;
+    ffmpeg_command += "'";
+    ffmpeg_command += " '";
+    ffmpeg_command += audio_name;
+    ffmpeg_command += "'";
+    std::cout << ffmpeg_command << std::endl;
+    system(ffmpeg_command.c_str());
+}
+
+
+int main(int argc, char *argv[]) {
+    CURL* curl = curl_easy_init();
+    CURLcode res;
+    std::string response;
+
+    Clients clients;
+
+    std::string body = get_body_to_video(clients, argv[1]);
+
+    struct curl_slist* headers = nullptr;
+    headers_to_player(headers);
+
+    make_ytplayer_request(curl, body, headers, res, response);
 
     std::ofstream file("url.txt");
     file << response;
     file.close();
 
-    //std::ifstream file("url.txt");
-    //
-    curl_easy_reset(curl);
+    std::string video_url = get_download_url(response);
 
-    nlohmann::json data = nlohmann::json::parse(response);
+    std::string video_title = get_video_title(response);
+    std::string vid_file_name = video_title;
+    vid_file_name += ".mp4";
 
-    //std::cout << data["streamingData"]["adaptiveFormats"].size() << std::endl;
-    std::vector<std::string> audio_urls = std::move(get_audio_download_url(data, data["streamingData"]["adaptiveFormats"].size()));
+    curl_download(curl, res, video_url, video_title);
 
-    bool audio_download_success = false;
 
     std::string aud_file_name = video_title;
 
     aud_file_name += ".mp3";
 
-    std::ofstream audio_file(aud_file_name, std::ios::binary);
-    for (auto& url : audio_urls){
-        struct curl_slist* audio_headers = nullptr;
-        audio_headers = curl_slist_append(audio_headers, "Content-Type: application/json");
-        audio_headers = curl_slist_append(audio_headers, "User-Agent: com.google.android.apps.youtube.vr.oculus/1.71.26 (Linux; U; Android 12L; eureka-user Build/SQ3A.220605.009.A1) gzip");
-        audio_headers = curl_slist_append(audio_headers, "X-YouTube-Client-Name: 28");
-        audio_headers = curl_slist_append(audio_headers, "X-YouTube-Client-Version: 1.71.26");
-
-        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_to_file);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &audio_file);
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, audio_headers);
-        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L); // follow redirects
-
-        res = curl_easy_perform(curl);
-
-        long httpCode = 0;
-        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpCode);
-        if (res != CURLE_OK) {
-            std::cerr << "curl error: " << curl_easy_strerror(res) << std::endl;
-        } else if (httpCode != 200) {
-            std::cerr << "HTTP error: " << httpCode << std::endl;  // will print 403
-        } else {
-            std::cout << url << std::endl;
-            std::cout << "Success!" << std::endl;
-            curl_slist_free_all(audio_headers);
-            audio_download_success = true;
-            break;
-        }
-        curl_slist_free_all(audio_headers);
-    }
-
-    audio_file.close();
-
-    if (audio_download_success != true){
-        std::string ffmpeg_command = "ffmpeg -i '";
-        ffmpeg_command += vid_file;
-        ffmpeg_command += "'";
-        ffmpeg_command += " '";
-        ffmpeg_command += aud_file_name;
-        ffmpeg_command += "'";
-        std::cout << ffmpeg_command << std::endl;
-        system(ffmpeg_command.c_str());
-    }
-
+    convert_to_audio(video_title, aud_file_name);
 
 
     curl_easy_cleanup(curl);
